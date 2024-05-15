673554777b7c9333a11b22f6a90018bb3bd49714
Vasilen Alexandrov <vasilen.alexandrov@endurosat.com>
2024-05-15 18:42:10 +0300

    Implemented transcribing files by chunks

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ from flask import Flask, flash, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 import whisper
 from googletrans import Translator
+import tempfile
+import io
+from pydub import AudioSegment
+import numpy as np
+import torch
 
 UPLOAD_FOLDER = os.getcwd() + '/uploads'
 ALLOWED_EXTENSIONS = {'mp3'}
+CHUNK_SIZE = 8 * 1024 * 1024
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+model = whisper.load_model("base")
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -28,18 +35,45 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            file_name = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
-            file.save(file_path)
+            global model
+            all_transcriptions = []
 
-            model = whisper.load_model("base")
-            result = model.transcribe(file_path)
-            print(result["text"])
+            # Create a BytesIO stream to buffer the uploaded file
+            audio_stream = io.BytesIO(file.read())
 
-            response = jsonify({'transcribedText': result["text"]})
+            try:
+                # Process the file in chunks
+                while True:
+                    # Create temporary audio file for this chunk
+                    with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as tmp:
+                        chunk = audio_stream.read(CHUNK_SIZE)
+
+                        # Break the loop if we have read all chunks
+                        if len(chunk) == 0:
+                            break
+
+                        # Write the chunk to the temporary file and transcribe it
+                        tmp.write(chunk)
+                        tmp.flush()  # Flush buffers to disk to ensure all data is written
+
+                        # Seek the temporary file to the beginning
+                        tmp.seek(0)
+
+                        # Transcribe the audio chunk
+                        result = model.transcribe(tmp.name)
+                        all_transcriptions.append(result["text"])
+                        print(f"Transcribed chunk: {tmp.name}")
+
+            except Exception as e:
+                return jsonify(error=str(e)), 500
+
+            # Combine all transcriptions into one
+            transcription = ''.join(all_transcriptions)
+            print(transcription)
+
+            response = jsonify({'transcribedText': transcription})
             response.headers.add('Access-Control-Allow-Origin', '*')
             return response
-
 @app.route('/translateText', methods=['POST'])
 def translate_text():
     if request.method == 'POST':
